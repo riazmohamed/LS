0a843f08787c67604274840c7660d147fdf77e2d
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-10 03:18:41 +0000

    ex3.rb List of movies

---
+++ intro-to-programming/the_basics/strings/ex3.rb
@@ -0,0 +1,14 @@
+# ex3.rb
+# list of movies
+
+movies = {
+          terminator: 1975,
+          robocop: 2004,
+          fist_of_fury: 2013,
+          enter_the_dragon: 2001,
+          the_young_master: 1981
+}
+
+movies.each do |k, v|
+  puts v
+end
