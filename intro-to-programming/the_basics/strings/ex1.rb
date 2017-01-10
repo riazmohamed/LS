0c24b4fbffe547b37353e72bf57916e16a147b89
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-10 02:50:53 +0000

    First commit the_basics

---
+++ intro-to-programming/the_basics/strings/ex1.rb
@@ -0,0 +1,10 @@
+# ex1.rb
+
+puts "Please enter your first name!"
+first_name = gets.chomp
+puts "Please enter your last name!"
+last_name = gets.chomp
+
+puts "#{first_name} " + "#{last_name}"
+puts "--------"
+puts "#{first_name} #{last_name}"
