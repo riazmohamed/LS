ec5ded03dd90d99fe3aed1f3c475a9bd3532aca6
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-11 15:27:26 +0000

    ex6.rb

---
+++ intro-to-programming/the_basics/variables/ex6.rb
@@ -0,0 +1,9 @@
+# ex6.rb
+
+# question
+# NameError: undefined local variable or method `shoes' for main:Object
+#   from (irb):3
+#   from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+# answer
+# here a variable or a method name 'shows' is not defined or being called outside the scope of this variable on line 3
