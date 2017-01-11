cc766f867ee93bfcf43c08687712992ce74998a8
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-11 20:31:19 +0000

    return.rb

---
+++ intro-to-programming/the_basics/methods/return.rb
@@ -0,0 +1,10 @@
+# return.rb
+
+def add_three(number)
+	number + 3
+end
+
+returned_value = add_three(4)
+puts returned_value
+
+# Ruby always returns the evaluated result of the last line of the expression unless an explicit return comes before it.
