b0d5e1a7d6041a17b9e845e0ed5a743cc8142432
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-11 20:35:25 +0000

    return.rb update

--- intro-to-programming/the_basics/methods/return.rb
+++ intro-to-programming/the_basics/methods/return.rb
@@ -9,3 +9,4 @@ returned_value = add_three(4)
 puts returned_value
 
 # Ruby always returns the evaluated result of the last line of the expression unless an explicit return comes before it.
+# The return reserved word is not required inorder to return something from a method.
