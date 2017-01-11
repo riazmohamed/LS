960936a4410836916da7dbfc440b7417072fdbe5
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-11 14:49:12 +0000

    Add capitalize to the ex1.rb

--- intro-to-programming/the_basics/variables/ex1.rb
+++ intro-to-programming/the_basics/variables/ex1.rb
@@ -1,6 +1,9 @@
 # ex1.rb
 
 puts "Please enter your name: "
-name = gets.chomp.capitalize
+name = gets.chomp
+split_name = name.split(" ")
+split_name.map! { |x| x.capitalize }
+name = split_name.join(" ")
 
 puts "Greetings, #{name}!"
