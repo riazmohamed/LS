e5ad60ae3f9c83cf88d5ce62d8a36b41521fb29e
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-11 15:11:07 +0000

    Print name 10 times by calling the .times method and also adding an alternative way to print them using loop

--- intro-to-programming/the_basics/variables/ex1.rb
+++ intro-to-programming/the_basics/variables/ex1.rb
@@ -1,9 +1,25 @@
 # ex1.rb
 
-puts "Please enter your name: "
+puts "Please enter your full name: "
 name = gets.chomp
 split_name = name.split(" ")
 split_name.map! { |x| x.capitalize }
 name = split_name.join(" ")
 
 puts "Greetings, #{name}!"
+puts "-------------"
+puts "Print name 10 times by calling the .times method"
+10.times { puts name }
+
+puts "-------------"
+puts "Alternative method using loop"
+
+count = 0
+loop do
+  count += 1
+  if count < 11
+    puts count.to_s + ". " + name
+  else
+    break
+  end
+end
