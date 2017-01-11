904a24b63d62402a229caf00d8f24a9cbf2feb52
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-11 17:27:03 +0000

    Code explaination added to say.rb

--- intro-to-programming/the_basics/methods/say.rb
+++ intro-to-programming/the_basics/methods/say.rb
@@ -1,10 +1,12 @@
 # say.rb refactored
 
 def say(words)
-  puts words
+  puts words + "." # changes made in one place takes effect througout the programe where the method say is called
 end
 
 say("hello")
 say("hi")
 say("how are you")
 say("I'm fine")
+
+# when we call the say("hello") for example: -  we pass in the string "hello" as an argument inplace of the parameter words in the say method. Then the code within the method is executed with the words variable evaluated to "hello".
