3f69d8778aee26a4bf1e3144a3622d10c298eead
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-11 21:02:24 +0000

    ex4, ex5 and ex6 added

--- intro-to-programming/the_basics/methods/exercises/ex4.rb
+++ intro-to-programming/the_basics/methods/exercises/ex4.rb
@@ -2,11 +2,10 @@
 
 def scream(words)
   words = words + "!!!!"
-  # return
+  return
   puts words
 end
 
 scream("Yippeee")
 
 # this returns nothing just an empty line.
-# when return is not explicitly defined it returns nil. Because in the last line puts evaluates to nil
