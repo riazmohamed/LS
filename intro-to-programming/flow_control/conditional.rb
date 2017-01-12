e95e654b6ced207114b5350558703d74b8e5a3b2
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-12 05:48:07 +0000

    more notes on conditionals

--- intro-to-programming/flow_control/conditional.rb
+++ intro-to-programming/flow_control/conditional.rb
@@ -10,3 +10,34 @@ elsif a == 4
 else
   puts "a is neither 3, nor 4"
 end
+
+# notes
+# The process of the program making the right decisions is known as conditional flow. This is acheived by using conditionals.
+
+# Examples of valid Ruby conditionals
+# Example 1
+if x == 3
+  puts "x is 3"
+end
+
+# Example 2
+if x == 3
+  puts "x is 3"
+elsif x == 4
+  puts "x is 4"
+end
+
+# Example 3
+if x == 3
+  puts "x is 3"
+else
+  puts "x is NOT 3"
+end
+
+# Example 4: must use "then" keyword when using 1-line syntax
+if x == 3 then puts "x is 3" end
+
+puts "x is 3" if x == 3
+
+puts "x is NOT 3" unless x == 3
+# here Ruby uses the reserved keyword unless which is the opposite of if statement.
