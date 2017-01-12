0974dc6a8ee93dc6b55e1e6727f823769048fcd9
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-12 07:43:10 +0000

    ex1.rb true or false

---
+++ intro-to-programming/flow_control/exercises/ex1.rb
@@ -0,0 +1,12 @@
+# ex1.rb true or false
+
+1. (32 * 4) >= 129
+    # false
+2. false != !true
+    # false
+3. true == 4
+    # false (here comparison operator is used and not assignment operator)
+4. false == (847 == '874')
+    # true (false == false)
+5. (!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false
+    # true
