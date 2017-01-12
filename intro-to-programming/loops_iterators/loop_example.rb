d7bd3acb98a8b80d78ecdb41b6e6ecc0a88567ac
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-12 09:27:46 +0000

    loops and iterators

---
+++ intro-to-programming/loops_iterators/loop_example.rb
@@ -0,0 +1,9 @@
+# loop_iterators.rb
+
+loop do
+  puts "This will keep printing until you hit Ctrl + c"
+end
+
+# notes
+
+# A loop is a repetitive execution of  a piece of code for a given amount of repetitons or until a certain conditions is met.
