2ab02afe5ae99206969d178c22dd1306091d0853
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-12 11:08:15 +0000

    wip

---
+++ intro-to-programming/loops_iterators/countdown3.rb
@@ -0,0 +1,11 @@
+# countdown3.rb
+
+x = gets.chomp.to_i
+
+for i in 1..x
+  puts i
+end
+
+puts "Done!"
+
+# The odd thing about the for loop is that the loop returns the collection of elements after it executes. Whereas the earlier loops returns nil
---
+++ intro-to-programming/loops_iterators/countdown4.rb
@@ -0,0 +1,12 @@
+# countdown4.rb
+
+x = [1, 2, 3, 4, 5]
+
+for i in x do
+  puts i
+end
+
+puts "Done!"
+
+# Here the value of x is an array with elements starting from 1 and ranging upto 5
+# Hence it returns the same as countdown3.rb where we defined the range within the for loop as i..x where x's value was 5
