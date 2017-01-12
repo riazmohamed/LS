0f794a68bff3af903065e3f69bf21a4462663d90
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-12 08:36:34 +0000

    ex6.rb missing end keyword

---
+++ intro-to-programming/flow_control/exercises/ex6.rb
@@ -0,0 +1,11 @@
+# ex6.rb
+
+    def equal_to_four(x)
+      if x == 4
+        puts "yup"
+      else
+        puts "nope"
+      end # this keyword end was missing hence the error
+    end
+
+    equal_to_four(5)
