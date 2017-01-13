5a576e96ae6d0a764049231a196793040a761d4d
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-13 22:50:19 +0000

    ex10.rb

---
+++ intro-to-programming/exercises/ex10.rb
@@ -0,0 +1,7 @@
+# ex10.rb
+
+# yes hash values can be arrays
+a = { key: [1, 2, 4, 5, 6, 7] }
+
+# you can also have an array of hashes
+a = [ { key: [1, 2, 4, 5, 6, 7] },  { something: [1, 2, 4, 5, 6, 7] }]
