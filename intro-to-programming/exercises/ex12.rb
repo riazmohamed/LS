51372702fa362b188f7b465fa0ac2ec86547a441
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-13 23:41:26 +0000

    ex12.rb

---
+++ intro-to-programming/exercises/ex12.rb
@@ -0,0 +1,11 @@
+# ex12.rb
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+contacts["Joe Smith"] = contact_data.first
+contacts["Sally Johnson"] = contact_data.last
+
+p contacts
