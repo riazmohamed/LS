7a512cb5a5e9be4a36ab2fad03cd219c9a8ae684
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-13 22:47:26 +0000

    ex9.rb

---
+++ intro-to-programming/exercises/ex9.rb
@@ -0,0 +1,14 @@
+# ex9.rb
+
+h = {a:1, b:2, c:3, d:4}
+
+# 1. Get the value of key `:b`.
+p h[:b] 
+
+# 2. Add to this hash the key:value pair `{e:5}`
+h[:e] = 5
+
+# 3. Remove all key:value pairs whose value is less than 3.5
+h.delete_if { |k, v| v < 3.5 }
+
+p h
