e3d93ec0e16bbc242a6ed14299e989cbf94bc6e4
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-13 04:38:01 +0000

    Add ex7.rb and ex8.rb

---
+++ intro-to-programming/hashes/exercises/ex5.rb
@@ -0,0 +1,9 @@
+# ex5.rb
+
+person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+
+if person.has_value?("painting")
+  puts "yes it is there"
+else
+  puts "no not there!"
+end
---
+++ intro-to-programming/hashes/exercises/ex7.rb
@@ -0,0 +1,9 @@
+# ex7.rb
+
+x = "hi there"
+my_hash = {x: "some value"} # here the key is a symbol
+my_hash2 = {x => "some value"} # here the key type used is a variable
+
+# ex8.
+
+# answer B
