73cd377b965921f7212a20f6ad58e654b3973404
Riaz Mohamed <railsdevriaz@gmail.com>
2017-01-14 00:28:54 +0000

    ex16.rb

---
+++ intro-to-programming/exercises/ex16.rb
@@ -0,0 +1,7 @@
+# ex16.rb
+
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+
+new_array = a.map { |word| word.split(" ") }
+p new_array.flatten!
